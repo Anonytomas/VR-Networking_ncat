c0c945a92ab243e1cfe6885c8d14d552dbc55138
Teapole <115593883+Teapole@users.noreply.github.com>
2024-05-06 19:01:12 -0400

    Fixes
    Desktop Orientations
    -- added phone
    -- added router
    -- added tablet
    -- added wirebuilder back desk
    -- modified script to accept up to two target renderers for wifi
    -- turned on the right three PCs by default

--- VR Networking 1/Assets/WireBuilder/Scripts/PlugController.cs
+++ VR Networking 1/Assets/WireBuilder/Scripts/PlugController.cs
@@ -20,7 +20,8 @@ public class PlugController : MonoBehaviour
 
     public Material connectedMaterial; // Material to apply when connected
     public Material unConnectedMaterial;
-    public Renderer targetRenderer; // Reference to the renderer of the specific object
+    public Renderer targetRenderer1; // Reference to the first renderer of the specific object
+    public Renderer targetRenderer2; // Reference to the second renderer of the specific object
     public int materialIndex = 1; // Index of the material element to change
 
     private void OnTriggerEnter(Collider other)
@@ -47,22 +48,39 @@ public class PlugController : MonoBehaviour
             Vector3 eulerRotation = new Vector3(this.transform.eulerAngles.x + 90, this.transform.eulerAngles.y, this.transform.eulerAngles.z);
             endAnchor.transform.rotation = Quaternion.Euler(eulerRotation);
 
-            // Change the material of the specific object's mesh renderer
-            if (targetRenderer != null && targetRenderer.materials.Length > materialIndex)
+            // Change the material of the specific object's mesh renderer 1
+            if (targetRenderer1 != null && targetRenderer1.materials.Length > materialIndex)
             {
-                Material[] materials = targetRenderer.materials;
-                materials[1] = connectedMaterial;
-                targetRenderer.materials = materials;
+                Material[] materials1 = targetRenderer1.materials;
+                materials1[1] = connectedMaterial;
+                targetRenderer1.materials = materials1;
+            }
+
+            // Change the material of the specific object's mesh renderer 2
+            if (targetRenderer2 != null && targetRenderer2.materials.Length > materialIndex)
+            {
+                Material[] materials2 = targetRenderer2.materials;
+                materials2[1] = connectedMaterial;
+                targetRenderer2.materials = materials2;
             }
 
         }
         else
         {
-            if (targetRenderer != null && targetRenderer.materials.Length > materialIndex)
+            // Change the material of the specific object's mesh renderer 1
+            if (targetRenderer1 != null && targetRenderer1.materials.Length > materialIndex)
             {
-                Material[] materials = targetRenderer.materials;
-                materials[materialIndex] = unConnectedMaterial;
-                targetRenderer.materials = materials;
+                Material[] materials1 = targetRenderer1.materials;
+                materials1[materialIndex] = unConnectedMaterial;
+                targetRenderer1.materials = materials1;
+            }
+
+            // Change the material of the specific object's mesh renderer 2
+            if (targetRenderer2 != null && targetRenderer2.materials.Length > materialIndex)
+            {
+                Material[] materials2 = targetRenderer2.materials;
+                materials2[materialIndex] = unConnectedMaterial;
+                targetRenderer2.materials = materials2;
             }
         }
     }
